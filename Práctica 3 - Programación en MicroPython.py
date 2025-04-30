6bbca78f45eaa5eacd6110f62e82dec47b4cbb59
Marko <zS23013998@estudiantes.uv.mx>
2025-04-29 22:45:57 -0600

    Add files via upload

---
+++ Práctica 3 - Programación en MicroPython.py
@@ -0,0 +1,38 @@
+import machine
+import time
+import dht
+
+# Inicialización de pines
+sensor_dht = dht.DHT11(machine.Pin(4))       # GP4
+pot = machine.ADC(26)                        # GP26 / ADC0
+
+# Archivo CSV
+archivo = "datos.csv"
+
+# Sobrescribir archivo al iniciar (limpiar datos anteriores y escribir encabezado)
+with open(archivo, "w") as f:
+    f.write("Fecha y Hora,Voltaje(V),Temperatura(C),Humedad(%)\n")
+
+while True:
+    # Leer ADC (potenciómetro)
+    pot_valor = pot.read_u16()               # Valor de 0 a 65535
+    voltaje = (pot_valor / 65535.0) * 3.3
+
+    # Leer DHT11
+    sensor_dht.measure()
+    temperatura = sensor_dht.temperature()
+    humedad = sensor_dht.humidity()
+    
+    #Guarda la fecha y hora (Timestamp)
+    timestamp = time.localtime()
+    año, mes, dia, hora, minuto, segundo, *_ = timestamp
+    timestamp_str = "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(año, mes, dia, hora, minuto, segundo)
+    
+    # Mostrar en terminal
+    print(f"Fecha y hora: {timestamp_str}| Voltaje: {voltaje:.2f} V | Temperatura: {temperatura} °C | Humedad: {humedad} %")
+
+    # Guardar en CSV solo si hay lectura válida
+    with open(archivo, "a") as f:
+        f.write(f"{timestamp_str}, {voltaje:.2f},{temperatura},{humedad}\n")
+
+    time.sleep(0.5)  # Leer cada medio segundo
