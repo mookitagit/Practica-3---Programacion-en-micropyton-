b75e49e2d0764c2014d2393af90c9512d11677cf
Marko <zS23013998@estudiantes.uv.mx>
2025-04-30 12:10:36 -0600

    Update Práctica 3 - Programación en MicroPython.py
    Se agrega sensor de sonido

--- Práctica 3 - Programación en MicroPython.py
+++ Práctica 3 - Programación en MicroPython.py
@@ -2,7 +2,7 @@
 
 # ESTE CÓDIGO EMPLEA MICROPYTHON PARA REALIZAR UNA LECTURA DE SENSORES EN RASPBAERRY PI PICO Y LOS GUARDA EN UN ARCHIVO CSV.
 
-# 29 / 04 / 2025 - V. 1. 0. 0 - INTEGRACIÓN DE SENSORES Y PROCESAMIENTO DE DATOS CON MICROPYTHON.
+# 30 / 04 / 2025 - V. 2. 0. 0 - INTEGRACIÓN DE SENSORES Y PROCESAMIENTO DE DATOS CON MICROPYTHON.
 
 import machine
 import time
@@ -11,13 +11,14 @@ import dht
 # Inicialización de pines
 sensor_dht = dht.DHT11(machine.Pin(4))       # GP4
 pot = machine.ADC(26)                        # GP26 / ADC0
+sensor_sonido = machine.ADC(27)              # GP27 / ADC1
 
 # Archivo CSV
 archivo = "datos.csv"
 
 # Sobrescribir archivo al iniciar (limpiar datos anteriores y escribir encabezado)
 with open(archivo, "w") as f:
-    f.write("Fecha y Hora,Voltaje(V),Temperatura(C),Humedad(%)\n")
+    f.write("Fecha y Hora,Voltaje(V),Temperatura(C),Humedad(%), Sonido\n")
 
 while True:
     # Leer ADC (potenciómetro)
@@ -29,16 +30,19 @@ while True:
     temperatura = sensor_dht.temperature()
     humedad = sensor_dht.humidity()
     
+    #Leer RQ-S001
+    sonido_valor = sensor_sonido.read_u16()
+    
     #Guarda la fecha y hora (Timestamp)
     timestamp = time.localtime()
     año, mes, dia, hora, minuto, segundo, *_ = timestamp
     timestamp_str = "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(año, mes, dia, hora, minuto, segundo)
     
     # Mostrar en terminal
-    print(f"Fecha y hora: {timestamp_str}| Voltaje: {voltaje:.2f} V | Temperatura: {temperatura} °C | Humedad: {humedad} %")
+    print(f"Fecha y hora: {timestamp_str}| Voltaje: {voltaje:.2f} V | Temperatura: {temperatura} °C | Humedad: {humedad}% | Sonido: {sonido_valor}")
 
     # Guardar en CSV solo si hay lectura válida
     with open(archivo, "a") as f:
-        f.write(f"{timestamp_str}, {voltaje:.2f},{temperatura},{humedad}\n")
+        f.write(f"{timestamp_str}, {voltaje:.2f},{temperatura},{humedad}, {sonido_valor}\n")
 
     time.sleep(0.5)  # Leer cada medio segundo
